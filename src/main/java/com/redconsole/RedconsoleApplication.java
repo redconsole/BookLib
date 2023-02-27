12f5ec326f3ac7b496400e884a700156d3308d8e
Narendra Kashyap <redconsolein@gmail.com>
2023-02-27 20:37:42 +0530

    initial commit 1

---
+++ src/main/java/com/redconsole/RedconsoleApplication.java
@@ -0,0 +1,13 @@
+package com.redconsole;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedconsoleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedconsoleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/redconsole/RedconsoleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.redconsole;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedconsoleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
